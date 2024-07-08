1646e54768af23a1e31b307c6d4ea1113dcbbcd7
MatthiasOs <ossi482+github@gmail.com>
2024-07-08 08:11:09 +0200

    dirties context in integration test

--- src/test/java/de/ossi/employeeservice/student/StudentControllerIntegrationTest.java
+++ src/test/java/de/ossi/employeeservice/student/StudentControllerIntegrationTest.java
@@ -1,9 +1,12 @@
 package de.ossi.employeeservice.student;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.assertj.core.api.Assertions;
 import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,14 +23,42 @@ class StudentControllerIntegrationTest {
 
     @Test
     void findByIdShouldReturnResult() {
-        assertThat(controller.getStudentById(1)).isNotNull();
+        assertThat(controller.getStudentById(10001)).isNotNull();
     }
 
+    @DirtiesContext
     @Test
     void deleteShouldRemoveStudent() {
-        assertThat(controller.getStudentById(4)).isNotNull();
-        controller.deleteById(4);
-        assertThat(controller.getAllStudents())
-                .areNot(new Condition<>(student -> student.getId().equals(4), "ID=4 Should not be found"));
+        Integer studentId = 10004;
+        assertThat(controller.getStudentById(studentId)).isNotNull();
+        controller.deleteById(studentId);
+        assertThatNoStudentWithIdFound(studentId);
+    }
+
+    @DirtiesContext
+    @Test
+    void createShouldAddNewStudent() {
+        String studentName = "NewStudent";
+        Condition<Student> shouldHaveLastName = new Condition<>(s -> "NewStudent".equals(s.getLastname()), "No Student should have Lastname=" + studentName);
+        assertThat(controller.getAllStudents()).areNot(shouldHaveLastName);
+        Student student = createStudent(studentName);
+        controller.createStudent(student);
+        assertThat(controller.getAllStudents()).areExactly(1, shouldHaveLastName);
+    }
+
+    private void assertThatNoStudentWithIdFound(Integer studentId) {
+        //TODO repo.existsById verwenden?
+        Assertions.assertThatThrownBy(() -> controller.getStudentById(studentId))
+                  .isInstanceOf(EntityNotFoundException.class)
+                  .hasMessageContaining("" + studentId);
+    }
+
+    private Student createStudent(String studentName) {
+        Student student = new Student();
+        student.setEmail("email" + studentName);
+        student.setAge(18);
+        student.setLastname(studentName);
+        student.setFirstname(studentName);
+        return student;
     }
 }
